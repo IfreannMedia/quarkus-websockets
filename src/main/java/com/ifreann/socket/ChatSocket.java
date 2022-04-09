ef7c746c4d907e3c5a833033ea3bca9b89f770c5
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-04-09 14:20:51 +0200

    added events and methods to ChatSocket

--- src/main/java/com/ifreann/socket/ChatSocket.java
+++ src/main/java/com/ifreann/socket/ChatSocket.java
@@ -1,10 +1,45 @@
 package com.ifreann.socket;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.util.concurrent.ConcurrentHashMap;
 
 @ServerEndpoint(value = "/chat/{username}")
 @ApplicationScoped
 public class ChatSocket {
 
+    private final ConcurrentHashMap<String, Session> sessionMap = new ConcurrentHashMap<String, Session>();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) {
+        this.sessionMap.put(username, session);
+        sendMessage(String.format("User %s logged in", username));
+    }
+
+    @OnMessage
+    public void onMessage(String message, @PathParam("username") String username) {
+        sendMessage(String.format(">> %s: %s", username, message));
+    }
+
+    @OnClose
+    public void onClose(Session session, @PathParam("username") String username) {
+        sessionMap.remove(username);
+        sendMessage(String.format("User %s logged out", username));
+    }
+
+    @OnError
+    public void onError(Session session, @PathParam("username") String username, Throwable throwable ) {
+        throwable.printStackTrace();
+        sendMessage(String.format("User %s logged out because of an error", username));
+    }
+
+    public void sendMessage(String message) {
+        sessionMap.values().forEach(it -> it.getAsyncRemote().sendObject(message, sendResult -> {
+            if (sendResult.getException() != null) {
+                sendResult.getException().printStackTrace();
+            }
+        }));
+    }
 }
